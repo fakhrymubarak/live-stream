acf91eb9bb4ff18e3d298318df5fb1cb673575a3
Fakhry Mubarak <fakhrymubarak@gmail.com>
2023-10-18 11:53:40 +0700

    feat: add new live streaming activity

---
+++ app/src/main/java/com/fakhry/livestream/LiveStreamingActivity.kt
@@ -0,0 +1,11 @@
+package com.fakhry.livestream
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LiveStreamingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_live_streaming)
+    }
+}
